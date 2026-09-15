3efa4e067fbe61acea6d33abcd13d14d96794aeb
youngheonchoi <gjswns21@gmail.com>
2026-09-15 19:44:55 +0900

    feat : add mvp phase1

--- src/main/java/com/okerry/okerry_diagram/OkerryDiagramApplication.java
+++ src/main/java/com/okerry/okerry_diagram/OkerryDiagramApplication.java
@@ -2,8 +2,10 @@ package com.okerry.okerry_diagram;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
+@MapperScan("com.okerry.okerry_diagram")
 public class OkerryDiagramApplication {
 
     public static void main(String[] args) {
